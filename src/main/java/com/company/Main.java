96a132459f04854feb7f756fab49a33486e989d5
Mihnea-Costache Marin <JustCosty21>
2021-01-19 15:11:25 +0200

    Program finished.

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,45 +1,28 @@
 package com.company;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.parser.Parser;
 
-import java.io.File;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
+    private final static XMLParser writer;
 
-    public static void main(String[] args) throws IOException {
-        List<Path> orderFiles = getOrderFiles();
+    static {
+        writer = new XMLParser();
+    }
 
-        for(Path path : orderFiles) {
-            Main.processFile(path);
+    public static void main(String[] args) throws IOException, TransformerException, ParserConfigurationException, SAXException {
+        List<Path> orderFiles = writer.getOrderFiles();
+
+        for (Path path : orderFiles) {
+            writer.processFile(path);
         }
     }
 
-    private static void processFile(Path path) throws IOException {
-        Document doc = Jsoup.parse(Files.readString(path), "", Parser.xmlParser());
-        for (Element e : doc.select("product")) {
-            System.out.println(e);
-        }
-    }
 
-    private static List<Path> getOrderFiles() {
-        List<Path> orderFiles = null;
-        try (Stream<Path> paths = Files.walk(Paths.get("./input_files"))) {
-            orderFiles = paths.filter(Files::isRegularFile)
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return orderFiles;
-    }
 }
